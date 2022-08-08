7daa0ef5ae451985a127d4a631073b55e6dc6538
alice <aliceahuh213@gmail.com>
2022-08-08 17:38:33 -0400

    add point value to rank enum

--- src/main/java/com/teambaccrat/model/Rank.java
+++ src/main/java/com/teambaccrat/model/Rank.java
@@ -2,29 +2,33 @@ package com.teambaccrat.model;
 
 public enum Rank {
 
-  ACE("A"),
-  TWO("2"),
-  THREE("3"),
-  FOUR("4"),
-  FIVE("5"),
-  SIX("6"),
-  SEVEN("7"),
-  EIGHT("8"),
-  NINE("9"),
-  TEN("10"),
-  JACK("J"),
-  QUEEN("Q"),
-  KING("K");
-
+  ACE("A", 1),
+  TWO("2", 2),
+  THREE("3", 3),
+  FOUR("4", 4),
+  FIVE("5", 5),
+  SIX("6", 6),
+  SEVEN("7", 7),
+  EIGHT("8", 8),
+  NINE("9", 9),
+  TEN("10", 10),
+  JACK("J", 0),
+  QUEEN("Q", 0),
+  KING("K", 0);
 
   private final String symbol;
+  private final int point;
 
-  Rank(String symbol) {
+  Rank(String symbol, int point) {
     this.symbol = symbol;
+    this.point = point;
   }
 
   public String getSymbol() {
     return symbol;
+  }
+  public int getPoint() {
+    return point;
   }
 }
 
