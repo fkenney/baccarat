9f9d7ffbf385a650f3b2ab4f825e79cf2bea2291
cwalker159 <cwalker159@gmail.com>
2022-08-08 15:29:09 -0700

    Setup Dealer class to deal cards.

--- src/main/java/com/teambaccrat/controller/Dealer.java
+++ src/main/java/com/teambaccrat/controller/Dealer.java
@@ -1,5 +1,31 @@
 package com.teambaccrat.controller;
 
-public class Dealer {
+import com.teambaccrat.model.Card;
+import java.util.ArrayList;
 
+
+public class Dealer {
+  ArrayList<Card> deck;
+  Dealer(){
+    deck = new ArrayList<Card>();
+  }
+
+
+  // Deal The Hand
+  public ArrayList<Card> dealHand(){
+    ArrayList<Card> newCard = new ArrayList<>();
+    Card card1 = deck.get(0);
+    Card card2 = deck.get(1);
+    newCard.add(card1);
+    newCard.add(card2);
+    deck.remove(0);
+    deck.remove(0);
+
+    return newCard;
+  }
+
+  // return deck size
+  public int deckSize(){
+    return this.deck.size(); // return the deck size
+  }
 }
