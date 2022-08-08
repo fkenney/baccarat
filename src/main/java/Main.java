da2013f8a337d494383e10a29c6e22802c8cf3ed
Felicia Kenney <fkenney@bravocg.com>
2022-08-08 15:52:56 -0400

    Added Main w/ draft project flow

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+  public static void main(String[] args) {
+
+    // Welcome Baccarat
+    // Please enter your name
+
+    // Build Deck
+    // Shuffle Deck with Random
+    // Create Player, Banker Hand
+    // Get Bets, 1 Player, 2 Banker, 3 Tie
+    // Deal one card to Player Hand
+    // Banker
+    // Player
+    // Banker
+    // Calculate points to determine 3rd Card
+    // Calculate points to determine winner
+    // Tell user if they won and give points.
+    // Would you like to play again?// Thanks for playing
+
+  }
+}
