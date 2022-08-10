999bfdba73bb62108bf0365b938fce0be732e764
cwalker159 <cwalker159@gmail.com>
2022-08-10 15:28:58 -0700

    Added print methods to view class

--- src/main/java/com/teambaccrat/view/View.java
+++ src/main/java/com/teambaccrat/view/View.java
@@ -1,7 +1,15 @@
 package com.teambaccrat.view;
 
+import com.teambaccrat.model.Game;
+
 public class View {
 
+  public void printBetInfo(Game game) {
+    System.out.println("You made a bet on " + game);
+  }
 
+  public void printAmountInfo(Game game) {
+    System.out.println("You bet $" + game);
+  }
 
 }
